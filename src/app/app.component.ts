3d491ea8f39af24330a507f0cedc4306b6055902
Agil Rajayyan <66911936+AgilRajayyan@users.noreply.github.com>
2024-02-05 02:00:24 +0530

    feat: Velocity Template Previewer
    -Added velocityjs and monaco-editor libraries
    -Added Inter font & a logo
    -Used the JSON data provided by the user to define the context for the velocity template thereby to render the HTML output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { parse, Compile } from 'velocityjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'velocity-template-previewer';
+export class AppComponent implements AfterViewInit {
+  templateEditorOptions = { theme: 'vs-dark', language: 'html' };
+  templateDataEditorOptions = { theme: 'vs-dark', language: 'json' };
+  velocityTemplate: string = `
+    <h2>Conditionals</h2>
+    #if ($renderIfBlock)
+        <p>If block is rendered since renderIf flag is true.</p>
+    #else
+        <p>Else block is rendered since renderIf flag is false.</p>
+    #end
+
+    <h2>Products</h2>
+    #if($products.size() > 0)
+      <table style="border-collapse: collapse;border: 2px solid rgb(140 140 140);">
+        <thead>
+            <tr>
+                <th style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">Name</th>
+                <th style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            #foreach($product in $products)
+            <tr>
+                <td style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">$product.name</td>
+                <td style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">$product.description</td>
+            </tr>
+            #end
+        </tbody>
+      </table>
+    #else
+      <p>No products found</p>
+    #end`;
+  templateData: string = `
+    {
+      "renderIfBlock": true,
+      "products": [
+          { "name": "Product A", "description": "Product A description" },
+          { "name": "Product B", "description": "Product B description" },
+          { "name": "Product C", "description": "Product C description" }
+      ]
+    }`;
+  @ViewChild('previewContainer') previewContainer!: ElementRef;
+  previewHtml: string = '';
+  previewType: 'HTML' | 'ACTUAL-OUTPUT' = 'ACTUAL-OUTPUT';
+
+  ngAfterViewInit(): void {
+    this.generatePreview();
+  }
+
+  generatePreview(): void {
+    const abstractSyntaxTree = parse(this.velocityTemplate);
+    const context = JSON.parse(this.templateData);
+    this.previewHtml = new Compile(abstractSyntaxTree).render(context);
+    this.updatePreviewContent();
+  }
+
+  onChangePreviewType(previewType: 'HTML' | 'ACTUAL-OUTPUT'): void {
+    this.previewType = previewType;
+    this.updatePreviewContent();
+  }
+
+  updatePreviewContent(): void {
+    if (this.previewType === 'HTML') {
+      this.previewContainer.nativeElement.innerText = this.previewHtml;
+    } else {
+      this.previewContainer.nativeElement.innerHTML = this.previewHtml;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MonacoEditorModule.forRoot(), FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
