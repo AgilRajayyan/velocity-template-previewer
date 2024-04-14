b5efa44f07a76c81eed64fc0b8c7ea6d1aa24d57
Agil Rajayyan <66911936+AgilRajayyan@users.noreply.github.com>
2024-04-15 00:26:52 +0530

    feat: Integrate spartan UI components
    -Used hlmBtn directive for the 'Generate Preview' button
    -Replaced the 'Show Preview'/'Show HTML' buttons with hlm-tabs-list
    -Added hlm-toaster for error toasts, it's shown if there's any parsing error
    -Changed the layout, so that there's much more space for the code editor and it's responsive
    -Updated the boilerplate content in the editors
    -Updated the favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { toast } from 'ngx-sonner';
 import { parse, Compile } from 'velocityjs';
 
 @Component({
@@ -9,69 +10,97 @@ import { parse, Compile } from 'velocityjs';
 export class AppComponent implements AfterViewInit {
   templateEditorOptions = { theme: 'vs-dark', language: 'html' };
   templateDataEditorOptions = { theme: 'vs-dark', language: 'json' };
-  velocityTemplate: string = `
-    <h2>Conditionals</h2>
-    #if ($renderIfBlock)
-        <p>If block is rendered since renderIf flag is true.</p>
-    #else
-        <p>Else block is rendered since renderIf flag is false.</p>
-    #end
-
-    <h2>Products</h2>
+  velocityTemplate: string = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Email Template</title>
+    <style>
+      body { font-family: Arial, sans-serif; }
+    </style>
+  </head>
+  <body>
+    <p>Hello there,</p>
+    <p>You could find two code editors above. One for the Velocity template and the other one for the template data in JSON format.</p>
+    <p>Once you're done with the Velocity template and the template data, click on the "Generate Preview" button.</p>
+    <p>Please note, it's not necessary to provide values for all the variables that you've in your template.</p>
+    <p>Feel free to play around.</p>
+    <h5>Example</h5>
     #if($products.size() > 0)
       <table style="border-collapse: collapse;border: 2px solid rgb(140 140 140);">
         <thead>
             <tr>
-                <th style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">Name</th>
-                <th style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">Description</th>
+              <th style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">Name</th>
+              <th style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">Price</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody>      
             #foreach($product in $products)
             <tr>
-                <td style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">$product.name</td>
-                <td style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">$product.description</td>
+              <td style="border: 1px solid rgb(160 160 160);padding: 8px 10px;">$product.name</td>
+              <td style="border: 1px solid rgb(160 160 160);padding: 8px 10px; text-align: right;">
+                #if ($product.strikeoffPrice)
+                  <span style="text-decoration: line-through; font-size: 12px; color: red;">$$product.strikeoffPrice</span>
+                #end
+                <span style="font-weight: bold;">$$product.price</span>
+              </td>
             </tr>
             #end
         </tbody>
       </table>
     #else
       <p>No products found</p>
-    #end`;
-  templateData: string = `
-    {
-      "renderIfBlock": true,
-      "products": [
-          { "name": "Product A", "description": "Product A description" },
-          { "name": "Product B", "description": "Product B description" },
-          { "name": "Product C", "description": "Product C description" }
-      ]
-    }`;
+    #end
+  </body>
+</html>`;
+  templateData: string = `{
+  "products": [
+      { "name": "Product A", "price": 110, "strikeoffPrice": 124 },
+      { "name": "Product B", "price": 45 },
+      { "name": "Product C", "price": 299 }
+  ]
+}`;
   @ViewChild('previewContainer') previewContainer!: ElementRef;
-  previewHtml: string = '';
-  previewType: 'HTML' | 'ACTUAL-OUTPUT' = 'ACTUAL-OUTPUT';
+  @ViewChild('htmlPreviewContainer') htmlPreviewContainer!: ElementRef;
 
   ngAfterViewInit(): void {
     this.generatePreview();
   }
 
   generatePreview(): void {
-    const abstractSyntaxTree = parse(this.velocityTemplate);
-    const context = JSON.parse(this.templateData);
-    this.previewHtml = new Compile(abstractSyntaxTree).render(context);
-    this.updatePreviewContent();
-  }
-
-  onChangePreviewType(previewType: 'HTML' | 'ACTUAL-OUTPUT'): void {
-    this.previewType = previewType;
-    this.updatePreviewContent();
-  }
-
-  updatePreviewContent(): void {
-    if (this.previewType === 'HTML') {
-      this.previewContainer.nativeElement.innerText = this.previewHtml;
-    } else {
-      this.previewContainer.nativeElement.innerHTML = this.previewHtml;
+    try {
+      const abstractSyntaxTree = parse(this.velocityTemplate);
+      try {
+        const context = JSON.parse(this.templateData);
+        const previewHtml: string = new Compile(abstractSyntaxTree).render(
+          context
+        );
+        this.updatePreviewContent(previewHtml);
+      } catch (templateDataError: any) {
+        this.showErrorToast(
+          'An error occured in Template Data',
+          templateDataError.message
+        );
+      }
+    } catch (templateError: any) {
+      this.showErrorToast(
+        'An error occured in Template',
+        templateError.message
+      );
     }
+  }
+
+  updatePreviewContent(previewHtml: string): void {
+    this.previewContainer.nativeElement.innerHTML = previewHtml;
+    this.htmlPreviewContainer.nativeElement.innerText = previewHtml;
+  }
+
+  showErrorToast(title: string, errorMessage: string): void {
+    toast(title, {
+      description: errorMessage,
+      action: {
+        label: 'Ok',
+        onClick: () => {},
+      },
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,29 @@ import { AppComponent } from './app.component';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { FormsModule } from '@angular/forms';
 
+import {
+  HlmTabsComponent,
+  HlmTabsContentDirective,
+  HlmTabsListComponent,
+  HlmTabsTriggerDirective,
+} from '@spartan-ng/ui-tabs-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmToasterComponent } from '@spartan-ng/ui-sonner-helm';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MonacoEditorModule.forRoot(), FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MonacoEditorModule.forRoot(),
+    FormsModule,
+    HlmButtonDirective,
+    HlmTabsComponent,
+    HlmTabsListComponent,
+    HlmTabsTriggerDirective,
+    HlmTabsContentDirective,
+    HlmToasterComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
